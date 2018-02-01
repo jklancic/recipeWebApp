3e00e54a0dc3b45b23dcfa6718b555e8f800d9cb
jklancic <jernej.klancic@appdirect.com>
2018-01-31 17:30:32 -0700

    Section 7 Lecture 81

---
+++ src/test/java/xyz/blackmonster/recipewebapp/RecipeWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package xyz.blackmonster.recipewebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipeWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
