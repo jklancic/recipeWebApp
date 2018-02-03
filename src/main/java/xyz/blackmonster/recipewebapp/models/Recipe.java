cabb12474b2393623fb61e52c3eb37d7c8ca69f8
jklancic <jernej.klancic@appdirect.com>
2018-02-02 19:36:50 -0700

    Section 8 Lecture 102

--- src/main/java/xyz/blackmonster/recipewebapp/models/Recipe.java
+++ src/main/java/xyz/blackmonster/recipewebapp/models/Recipe.java
@@ -10,7 +10,10 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -46,6 +49,12 @@ public class Recipe {
 	
 	@Enumerated(value = EnumType.STRING)
 	private Difficulty difficulty;
+	
+	@ManyToMany
+	@JoinTable(name = "recipe_category", 
+		joinColumns = @JoinColumn(name = "recipe_id"),
+		inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories;
 
 	public long getId() {
 		return id;
@@ -141,5 +150,13 @@ public class Recipe {
 
 	public void setDifficulty(Difficulty difficulty) {
 		this.difficulty = difficulty;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
 	}
 }
