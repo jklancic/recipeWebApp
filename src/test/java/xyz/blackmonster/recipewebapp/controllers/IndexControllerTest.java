2b1e50f5083435f9a6a960d4d255a56ee03d2903
jklancic <jernej.klancic@appdirect.com>
2018-02-05 20:42:13 -0700

    Section 8 Lecture 125

--- src/test/java/xyz/blackmonster/recipewebapp/controllers/IndexControllerTest.java
+++ src/test/java/xyz/blackmonster/recipewebapp/controllers/IndexControllerTest.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
@@ -41,15 +42,23 @@ public class IndexControllerTest {
 	@Test
 	public void getIndexPage() {
 		List<Recipe> recipes = new ArrayList<>();
-		Recipe recipe = new Recipe();
-		recipe.setName("name");
+		Recipe recipe1 = new Recipe();
+		recipe1.setName("name1");
+		Recipe recipe2 = new Recipe();
+		recipe2.setName("name1");
+		recipes.add(recipe1);
+		recipes.add(recipe2);
 		when(recipeService.getAllRecipes()).thenReturn(recipes);
 		when(model.addAttribute(anyString(), anyList())).thenReturn(model);
+
+		ArgumentCaptor<List<Recipe>> argumentCaptor = ArgumentCaptor.forClass(List.class);
 		
 		String viewName = indexController.getIndexPage(model);
 		
 		assertEquals(viewName, IndexController.INDEX);
 		verify(recipeService, times(1)).getAllRecipes();
-		verify(model, times(1)).addAttribute(eq("recipes"), anyList());
+		verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+		List<Recipe> setInController = argumentCaptor.getValue();
+		assertEquals(recipes.size(), setInController.size());
 	}
 }
