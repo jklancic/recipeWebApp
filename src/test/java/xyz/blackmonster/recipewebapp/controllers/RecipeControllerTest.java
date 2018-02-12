7da16f4452ef5779211b14204333ddf0446ce2ff
jklancic <jernej.klancic@appdirect.com>
2018-02-12 16:16:08 -0700

    Section 11 Lecture 135

--- src/test/java/xyz/blackmonster/recipewebapp/controllers/RecipeControllerTest.java
+++ src/test/java/xyz/blackmonster/recipewebapp/controllers/RecipeControllerTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,7 +13,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
 
 import xyz.blackmonster.recipewebapp.models.Recipe;
 import xyz.blackmonster.recipewebapp.services.RecipeService;
@@ -21,9 +21,6 @@ public class RecipeControllerTest {
 
 	@Mock
 	private RecipeService recipeService;
-
-	@Mock
-	private Model model;
 
 	private RecipeController recipeController;
 
@@ -47,6 +44,7 @@ public class RecipeControllerTest {
 		
 		mockMvc.perform(get("/recipe/show/1"))
 			.andExpect(status().isOk())
-			.andExpect(view().name("recipe/show"));
+			.andExpect(view().name("recipe/show"))
+			.andExpect(model().attributeExists("recipe"));
 	}
 }
