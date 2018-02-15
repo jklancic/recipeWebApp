9139cda086f877d640844b288a56d5a5f4619b1d
jklancic <jernej.klancic@appdirect.com>
2018-02-14 21:12:09 -0700

    Section 11 Lecture 138

--- src/main/java/xyz/blackmonster/recipewebapp/controllers/RecipeController.java
+++ src/main/java/xyz/blackmonster/recipewebapp/controllers/RecipeController.java
@@ -3,9 +3,12 @@ package xyz.blackmonster.recipewebapp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import xyz.blackmonster.recipewebapp.commands.RecipeCommand;
 import xyz.blackmonster.recipewebapp.models.Recipe;
 import xyz.blackmonster.recipewebapp.services.RecipeService;
 
@@ -26,5 +29,20 @@ public class RecipeController {
 		model.addAttribute("recipe", recipe);
 		
 		return "recipe/show";
+	}
+	
+	@RequestMapping("recipe/new")
+	public String newRecipe(Model model) {
+		
+		model.addAttribute("recipe", new RecipeCommand());
+		return "recipe/recipeform";
+	}
+	
+	@PostMapping
+	@RequestMapping("recipe")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+		
+		RecipeCommand savedRecipe = recipeService.saveRecipeCommand(recipeCommand);
+		return "redirect:/recipe/show/" + savedRecipe.getId();
 	}
 }
