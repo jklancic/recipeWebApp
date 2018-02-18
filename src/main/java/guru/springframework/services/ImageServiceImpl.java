981068bc912dab3ad9f5d79644928251c529f212
jklancic <jernej.klancic@appdirect.com>
2018-02-18 13:04:15 -0700

    Section 11 Lecture 150

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -1,6 +1,9 @@
 package guru.springframework.services;
 
+import java.io.IOException;
 import java.util.Optional;
+
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
 
+@Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
 	
@@ -19,9 +23,23 @@ public class ImageServiceImpl implements ImageService {
 
 	@Override
 	public void saveFileImage(Long id, MultipartFile file) {
-		Optional<Recipe> recipe = recipeRepository.findById(id);
-		if(recipe.isPresent()) {
-			
+		Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+		if(recipeOptional.isPresent()) {
+			try {
+				Byte[] byteObjects = new Byte[file.getBytes().length];
+				int index = 0;
+				for (byte b: file.getBytes()) {
+					byteObjects[index++] = b;
+				}
+				
+				Recipe recipe = recipeOptional.get();
+				recipe.setImage(byteObjects);
+				recipeRepository.save(recipe);
+				log.info("Image saved.");
+			} catch (IOException e) {
+				log.error("Image file is bad:", e.getMessage());
+				log.error(e.toString());
+			}
 		}
 	}
 }
